58d1361fc95dbcff7c288492754da953c531a30c
张洪宇 <aroenzhang@sina.com>
2016-11-07 17:03:03 +0800

    修改iiii

--- app/src/main/java/aroen/com/studiopushtest/MainActivity.java
+++ app/src/main/java/aroen/com/studiopushtest/MainActivity.java
@@ -1,8 +1,7 @@
 package aroen.com.studiopushtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = new TextView(this);
-        tv.setText(R.string.app_name);
     }
 }
